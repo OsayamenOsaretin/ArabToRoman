24562ff746083243f59b9f802978f05d98b237f6
yamenO <yamen@jtpk.co>
2019-11-05 06:46:07 +0100

    add docstrings

--- function.py
+++ function.py
@@ -7,25 +7,28 @@ roman_numerals_mapping = {
 
 
 def value_converter(value, denominator):
+    """ Converts the integer value given using the denominator
+    
+    >>> [value_converter(number, 100) for number in [1,5,9]]
+    ['C', 'D', 'CM']
+    >>> [value_converter(number, 10) for number in [1,5,9]]
+    ['X', 'L', 'XC']
+    """
     romans_at_denominator = roman_numerals_mapping[denominator]
 
     if denominator == 1000:
         return value * romans_at_denominator[0]
 
     if value == 4:
-        # return denominator value for 4
         roman_value = ''.join(romans_at_denominator[:2])
         return roman_value
     if value == 9:
-        # return denominator value for 9
         roman_value = ''.join(romans_at_denominator[0::2])
         return roman_value
     if value < 5:
-        # return unit value * value
         roman_value = romans_at_denominator[0] * value
         return roman_value
     if value >= 5:
-        # return denominator value for 5 + value_converter(value - 5, denominator/10)
         five_value = romans_at_denominator[1]
         roman_value = five_value + value_converter(value - 5, denominator)
         return roman_value
@@ -43,6 +46,12 @@ def arabic_to_roman_converter(number, denominator=1000):
 
 
 def validate_argument(maybe_number):
+    """ Validates arguments to make sure they are valid integers
+
+    >>> [validate_argument(maybe_number) for maybe_number in ['10', 'invalid string', '4001']]
+    [(10, None), (None, 'Invalid argument, value must be an Integer between 1 and 3999'), (None, 'Invalid argument, value must be an Integer between 1 and 3999')]
+
+    """
     try:
         value = int(maybe_number)
         if value < 1 or value > 3999:
